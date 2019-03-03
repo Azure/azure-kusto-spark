a74251da315aff4862750c0c0003f4fe5a8aad79
Ohad Bitton <ohbitton@microsoft.com>
2019-03-03 11:56:58 +0200

    keyVault fix for databricks

--- connector/src/main/scala/com/microsoft/kusto/spark/datasink/TempStorageCache.scala
+++ connector/src/main/scala/com/microsoft/kusto/spark/datasink/TempStorageCache.scala
@@ -5,14 +5,16 @@ import java.util
 import com.microsoft.azure.kusto.data.{Client, ConnectionStringBuilder}
 import com.microsoft.kusto.spark.datasource.{KustoAuthentication, KustoCoordinates}
 import com.microsoft.kusto.spark.utils.CslCommandsGenerator._
-import com.microsoft.kusto.spark.utils.KustoClient
+import com.microsoft.kusto.spark.utils.{KustoClient, KustoDataSourceUtils}
 import org.joda.time.{DateTime, DateTimeZone, Period}
+
+import scala.collection.JavaConverters._
 
 object TempStorageCache{
 
   var roundRubinIdx = 0
   var cluster = ""
-  var storages = new util.ArrayList[String]
+  var storages = new Array[String](0)
   var dmClient: Client = _
   var lastRefresh: DateTime = new DateTime(DateTimeZone.UTC)
 
@@ -24,17 +26,22 @@ object TempStorageCache{
 
   private def getNextUri(clusterAlias: String, kcsb: ConnectionStringBuilder): String = {
     // Refresh if 120 minutes have passed since last refresh for this cluster as SAS should be valid for at least 120 minutes
-    if(storages.size() == 0 || cluster != clusterAlias || new Period(new DateTime(DateTimeZone.UTC), lastRefresh).getMinutes > storageExpiryMinutes){
+    if(storages.length == 0 ||  new Period(new DateTime(DateTimeZone.UTC), lastRefresh).getMinutes > storageExpiryMinutes){
+      if(cluster != clusterAlias)
+      {
+        KustoDataSourceUtils.logWarn("TempStorageCache", "Code should not get here")
+      }
       dmClient = KustoClient.getAdmin(kcsb)
+
       cluster = clusterAlias
 
       lastRefresh = new DateTime(DateTimeZone.UTC)
 
       val res = dmClient.execute(generateCreateTmpStorageCommand())
-      storages = res.getValues.get(0)
+      storages = res.getValues.asScala.map(row => row.get(0)).toArray
     }
 
-    roundRubinIdx = (roundRubinIdx + 1) % storages.size
-    storages.get(roundRubinIdx)
+    roundRubinIdx = (roundRubinIdx + 1) % storages.length
+    storages(roundRubinIdx)
   }
 }
