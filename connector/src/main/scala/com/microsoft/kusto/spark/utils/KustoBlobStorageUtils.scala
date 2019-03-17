ed35bb95c7ba107582658596dfd90b3e70edf3d9
michazag <42432661+michazag@users.noreply.github.com>
2019-03-17 22:26:15 +0200

    Fixes and styling

--- connector/src/main/scala/com/microsoft/kusto/spark/utils/KustoBlobStorageUtils.scala
+++ connector/src/main/scala/com/microsoft/kusto/spark/utils/KustoBlobStorageUtils.scala
@@ -3,6 +3,7 @@ package com.microsoft.kusto.spark.utils
 import com.microsoft.azure.storage.CloudStorageAccount
 import com.microsoft.azure.storage.blob.CloudBlockBlob
 import com.microsoft.kusto.spark.utils.{KustoDataSourceUtils => KDSU}
+import scala.collection.JavaConversions._
 
 object KustoBlobStorageUtils {
   def deleteFromBlob(account: String, directory: String, container: String, secret: String, keyIsSas: Boolean = false): Unit = {
@@ -30,20 +31,15 @@ object KustoBlobStorageUtils {
     performBlobDelete(directory, storageParams.container, storageConnectionString)
   }
 
-  private[kusto] def performBlobDelete(directory: String, container: String, storageConnectionString: String) = {
+  private[kusto] def performBlobDelete(directory: String, container: String, storageConnectionString: String): Unit = {
     val cloudStorageAccount = CloudStorageAccount.parse(storageConnectionString)
 
     val blobClient = cloudStorageAccount.createCloudBlobClient()
     val blobContainer = blobClient.getContainerReference(container)
-
-    import scala.collection.JavaConversions._
-    val blobsWithPrefix = blobContainer.listBlobs(directory).iterator().toList
+    val blobsWithPrefix = blobContainer.listBlobs(directory)
 
     blobsWithPrefix.foreach(blob => {
-      val uri = blob.getUri.toString
-
-      val directoryIndex = uri.indexOf(directory)
-      val cloudBlob = blobContainer.getBlockBlobReference((new CloudBlockBlob(blob.getUri)).getName)
+      val cloudBlob = blobContainer.getBlockBlobReference(new CloudBlockBlob(blob.getUri).getName)
       cloudBlob.deleteIfExists()
     })
   }
