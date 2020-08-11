321364274a300498d9bc54fad5b97322a784acf3
Abid Mohammed <abmo@microsoft.com>
2020-08-11 12:57:01 -0700

    Migrate to MSAL4J

--- connector/src/main/scala/com/microsoft/kusto/spark/authentication/DeviceAuthentication.scala
+++ connector/src/main/scala/com/microsoft/kusto/spark/authentication/DeviceAuthentication.scala
@@ -1,115 +1,95 @@
 package com.microsoft.kusto.spark.authentication
 
-import java.awt.{Desktop, Toolkit}
 import java.awt.datatransfer.{DataFlavor, StringSelection}
+import java.awt.{Desktop, Toolkit}
 import java.net.URI
 import java.util.concurrent.{ExecutionException, ExecutorService, Executors}
+import java.util.function.Consumer
 
-import com.microsoft.aad.adal4j.{AuthenticationContext, AuthenticationResult, DeviceCode}
+import com.microsoft.aad.msal4j.{DeviceCode, DeviceCodeFlowParameters, IAuthenticationResult, PublicClientApplication}
 import javax.naming.ServiceUnavailableException
 import org.apache.log4j.{Level, Logger}
 
+import scala.collection.JavaConverters._
 import scala.util.Try
 
 object DeviceAuthentication {
   // This is the Kusto client id from the java client used for device authentication.
   private val CLIENT_ID = "db662dc1-0cfe-4e1c-a843-19a68e65be58"
 
-  def acquireDeviceCode(clusterUrl: String, authority: String = "common"): DeviceCode = {
-    var aadAuthorityUri = ""
+  private def getAadAuthorityUrl(authority: String): String = {
     val aadAuthorityFromEnv = System.getenv("AadAuthorityUri")
     if (aadAuthorityFromEnv == null) {
-      aadAuthorityUri = String.format("https://login.microsoftonline.com/%s", authority)
+      String.format("https://login.microsoftonline.com/%s", authority)
     } else {
-      aadAuthorityUri = String.format("%s%s%s", aadAuthorityFromEnv, if (aadAuthorityFromEnv.endsWith("/")) "" else "/", authority)
+      String.format("%s%s%s", aadAuthorityFromEnv, if (aadAuthorityFromEnv.endsWith("/")) "" else "/", authority)
     }
-
-    val service = Executors.newSingleThreadExecutor
-    val context: AuthenticationContext =  new AuthenticationContext(aadAuthorityUri, true, service)
-
-    context.acquireDeviceCode(CLIENT_ID, clusterUrl, null).get
   }
 
-  def acquireAccessTokenUsingDeviceCodeFlow(clusterUrl: String, authority: String = "common", userDeviceCode: Option[DeviceCode] = None) : String = {
-    val deviceCode = if (userDeviceCode.isDefined) userDeviceCode.get else acquireDeviceCode(clusterUrl, authority)
-    val aadAuthorityUri = s"https://login.microsoftonline.com/$authority"
-    val service: ExecutorService =
-      Executors.newSingleThreadExecutor
-    val context: AuthenticationContext =  new AuthenticationContext(aadAuthorityUri, true, service)
+  def acquireAccessTokenUsingDeviceCodeFlow(clusterUrl: String, authority: String = "common") : String = {
+    val service: ExecutorService = Executors.newSingleThreadExecutor
+    try{
+      val publicClientApp =  PublicClientApplication.builder(CLIENT_ID)
+        .authority(getAadAuthorityUrl(authority))
+        .executorService(service)
+        .build();
 
-    var text: String = null
-    Try {
-      val clipboard = Toolkit.getDefaultToolkit.getSystemClipboard
-      val dataFlavor = DataFlavor.stringFlavor
-      if (clipboard.isDataFlavorAvailable(dataFlavor)) {
-        text = clipboard.getData(dataFlavor).asInstanceOf[String]
-      }
-      clipboard.setContents(new StringSelection(deviceCode.getUserCode), null)
+      val result = waitAndAcquireTokenByDeviceCode(publicClientApp, clusterUrl)
+      if (result == null) throw new ServiceUnavailableException("authentication result was null")
+      result.accessToken()
+    }finally {
+      service.shutdown()
     }
-
-    println(deviceCode.getMessage +
-      (if (!Desktop.isDesktopSupported) deviceCode
-      else " device code is already copied to clipboard - just press ctrl+v in the web"))
-    if (Desktop.isDesktopSupported) Desktop.getDesktop.browse(new URI(deviceCode.getVerificationUrl))
-    val result = waitAndAcquireTokenByDeviceCode(deviceCode, context)
-    if(text != null) {
-      Try {
-        val clipboard = Toolkit.getDefaultToolkit.getSystemClipboard
-        clipboard.setContents(new StringSelection(text), null)
-      }
-    }
-    if (result == null) throw new ServiceUnavailableException("authentication result was null")
-    result.getAccessToken
   }
 
   @throws[InterruptedException]
-  private def waitAndAcquireTokenByDeviceCode(deviceCode: DeviceCode, context: AuthenticationContext): AuthenticationResult = {
+  private def waitAndAcquireTokenByDeviceCode(publicClientApp: PublicClientApplication, clusterUrl: String): IAuthenticationResult = {
     var timeout = 15 * 1000
-    var result: AuthenticationResult = null
+    var result: IAuthenticationResult = null
 
     // Logging is set to Fatal as root
-    val prevLevel = Logger.getLogger(classOf[AuthenticationContext]).getLevel
+    val prevLevel = Logger.getLogger(classOf[PublicClientApplication]).getLevel
     Thread.sleep(5000)
-    Logger.getLogger(classOf[AuthenticationContext]).setLevel(Level.FATAL)
+    Logger.getLogger(classOf[IAuthenticationResult]).setLevel(Level.FATAL)
     while ( result == null && timeout > 0) {
       try {
-        result = context.acquireTokenByDeviceCode(deviceCode, null).get
-
+        val deviceCodeFlowParams = DeviceCodeFlowParameters.builder(Set(clusterUrl+"/.default").asJava,DeviceCodeConsumer).build()
+        result = publicClientApp.acquireToken(deviceCodeFlowParams).get()
       } catch {
         case e: ExecutionException =>
           Thread.sleep(1000)
           timeout -= 1000
       }
     }
-
-    Logger.getLogger(classOf[AuthenticationContext]).setLevel(prevLevel)
+    Logger.getLogger(classOf[PublicClientApplication]).setLevel(prevLevel)
     result
   }
-}
 
-// This class helps using device authentication in pyspark
-class DeviceAuthentication(val clusterUrl: String, val authority: String = "common") {
-  import DeviceAuthentication._
+  private def DeviceCodeConsumer: Consumer[DeviceCode] = {
+    new Consumer[DeviceCode] {
+      override def accept(t: DeviceCode): Unit = {
+        var text: String = null
+        Try {
+          val clipboard = Toolkit.getDefaultToolkit.getSystemClipboard
+          val dataFlavor = DataFlavor.stringFlavor
+          if (clipboard.isDataFlavorAvailable(dataFlavor)) {
+            text = clipboard.getData(dataFlavor).asInstanceOf[String]
+          }
+          clipboard.setContents(new StringSelection(t.userCode()), null)
+        }
 
-  var currentDeviceCode: Option[DeviceCode] = None
-
-  def getDeviceCodeMessage: String = {
-    if (currentDeviceCode.isEmpty) {
-      refreshDeviceCode()
+        println(t.message() +
+          (if (!Desktop.isDesktopSupported) t
+          else " device code is already copied to clipboard - just press ctrl+v in the web"))
+        if (Desktop.isDesktopSupported) Desktop.getDesktop.browse(new URI(t.verificationUri()))
+        if (text != null) {
+          Try {
+            val clipboard = Toolkit.getDefaultToolkit.getSystemClipboard
+            clipboard.setContents(new StringSelection(text), null)
+          }
+        }
+        Thread.sleep(10000)
+      }
     }
-
-    currentDeviceCode.get.getMessage
-  }
-
-  def acquireToken(): String = {
-    if (currentDeviceCode.isEmpty) {
-      refreshDeviceCode()
-    }
-
-    acquireAccessTokenUsingDeviceCodeFlow(clusterUrl, authority, currentDeviceCode)
-  }
-
-  def refreshDeviceCode(): Unit = {
-    currentDeviceCode = Some(acquireDeviceCode(clusterUrl, authority))
   }
 }
--- connector/src/main/scala/com/microsoft/kusto/spark/utils/KeyVaultUtils.scala
+++ connector/src/main/scala/com/microsoft/kusto/spark/utils/KeyVaultUtils.scala
@@ -2,8 +2,7 @@ package com.microsoft.kusto.spark.utils
 
 import java.io.IOException
 
-import com.microsoft.azure.CloudException
-import com.microsoft.azure.keyvault.KeyVaultClient
+import com.azure.security.keyvault.secrets.SecretClient
 import com.microsoft.kusto.spark.authentication._
 import com.microsoft.kusto.spark.datasource._
 
@@ -15,53 +14,51 @@ object KeyVaultUtils {
   val StorageAccountId = "blobStorageAccountName"
   val StorageAccountKey = "blobStorageAccountKey"
   val Container = "blobContainer"
-  var cachedClient: KeyVaultClient = _
+  var cachedClient: SecretClient = _
 
-  private def getClient(clientID: String, clientPassword: String): KeyVaultClient ={
+  private def getClient(uri: String, clientID: String, clientPassword: String): SecretClient ={
     if(cachedClient == null) {
-      cachedClient = new KeyVaultADALAuthenticator(clientID, clientPassword).getAuthenticatedClient
+      cachedClient = new KeyVaultADALAuthenticator(uri, clientID, clientPassword).getAuthenticatedClient
     }
     cachedClient
   }
 
-  @throws[CloudException]
   @throws[IOException]
   def getStorageParamsFromKeyVault(keyVaultAuthentication: KeyVaultAuthentication): KustoStorageParameters = {
     keyVaultAuthentication match {
       case app: KeyVaultAppAuthentication =>
-        val client = getClient(app.keyVaultAppID, app.keyVaultAppKey)
+        val client = getClient(app.uri, app.keyVaultAppID, app.keyVaultAppKey)
         getStorageParamsFromKeyVaultImpl(client, app.uri)
       case certificate: KeyVaultCertificateAuthentication => throw new UnsupportedOperationException("certificates are not yet supported")
     }
   }
 
-  @throws[CloudException]
   @throws[IOException]
   def getAadAppParametersFromKeyVault(keyVaultAuthentication: KeyVaultAuthentication): AadApplicationAuthentication={
     keyVaultAuthentication match {
       case app: KeyVaultAppAuthentication =>
-        val client = getClient(app.keyVaultAppID, app.keyVaultAppKey)
+        val client = getClient(app.uri, app.keyVaultAppID, app.keyVaultAppKey)
         getAadAppParamsFromKeyVaultImpl(client, app.uri)
       case certificate: KeyVaultCertificateAuthentication => throw new UnsupportedOperationException("certificates are not yet supported")
     }
   }
 
-  private def getAadAppParamsFromKeyVaultImpl(client: KeyVaultClient, uri: String): AadApplicationAuthentication ={
+  private def getAadAppParamsFromKeyVaultImpl(client: SecretClient, uri: String): AadApplicationAuthentication ={
     val id = client.getSecret(uri, AppId)
     val key = client.getSecret(uri, AppKey)
 
-    var authority = client.getSecret(uri, AppAuthority).value()
+    var authority = client.getSecret(uri, AppAuthority).getValue()
     if(authority.isEmpty){
       authority = "microsoft.com"
     }
 
     AadApplicationAuthentication(
-      ID = if (id == null) null else id.value(),
-      password = if (key == null) null else key.value(),
+      ID = if (id == null) null else id.getValue(),
+      password = if (key == null) null else key.getValue(),
       authority = authority)
   }
 
-  private def getStorageParamsFromKeyVaultImpl(client: KeyVaultClient, uri: String): KustoStorageParameters = {
+  private def getStorageParamsFromKeyVaultImpl(client: SecretClient, uri: String): KustoStorageParameters = {
     val sasUrl = Option(client.getSecret(uri, SasUrl))
 
     val accountId =  Option(client.getSecret(uri, StorageAccountId))
@@ -70,12 +67,12 @@ object KeyVaultUtils {
 
     if(sasUrl.isEmpty) {
       KustoStorageParameters(
-        account = if(accountId.isDefined) accountId.get.value else "",
-        secret = if (accountKey.isDefined) accountKey.get.value else "",
-        container = if (container.isDefined) container.get.value else "",
+        account = if(accountId.isDefined) accountId.get.getValue else "",
+        secret = if (accountKey.isDefined) accountKey.get.getValue else "",
+        container = if (container.isDefined) container.get.getValue else "",
         secretIsAccountKey = true)
     } else {
-      KustoDataSourceUtils.parseSas(sasUrl.get.value)
+      KustoDataSourceUtils.parseSas(sasUrl.get.getValue)
     }
   }
 }
