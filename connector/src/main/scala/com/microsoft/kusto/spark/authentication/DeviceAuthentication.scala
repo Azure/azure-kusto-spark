7a6bf88dc5ca80057cef3d1eaa770a8f8488b0fb
ohbitton <ohad.bitton@mail.huji.ac.il>
2021-03-14 10:05:47 +0200

    device

---
+++ connector/src/main/scala/com/microsoft/kusto/spark/authentication/DeviceAuthentication.scala
@@ -0,0 +1,120 @@
+package com.microsoft.kusto.spark.authentication
+
+import java.awt.{Desktop, Toolkit}
+import java.awt.datatransfer.{DataFlavor, StringSelection}
+import java.net.URI
+import java.util.concurrent.{ExecutionException, ExecutorService, Executors}
+
+import com.microsoft.aad.adal4j.{AuthenticationContext, AuthenticationResult}
+import com.microsoft.aad.msal4j._
+import com.microsoft.aad.msal4j.DeviceCodeFlowParameters
+import com.microsoft.aad.msal4j.IAuthenticationResult
+import com.microsoft.aad.msal4j.PublicClientApplication
+import java.util.concurrent.CompletableFuture
+import javax.naming.ServiceUnavailableException
+import org.apache.log4j.{Level, Logger}
+
+import scala.util.Try
+
+object DeviceAuthentication {
+  // This is the Kusto client id from the java client used for device authentication.
+  private val CLIENT_ID = "db662dc1-0cfe-4e1c-a843-19a68e65be58"
+
+  def acquireDeviceCode(clusterUrl: String, authority: String = "common"): PublicClientApplication = {
+    var aadAuthorityUri = ""
+    val aadAuthorityFromEnv = System.getenv("AadAuthorityUri")
+    if (aadAuthorityFromEnv == null) {
+      aadAuthorityUri = String.format("https://login.microsoftonline.com/%s", authority)
+    } else {
+      aadAuthorityUri = String.format("%s%s%s", aadAuthorityFromEnv, if (aadAuthorityFromEnv.endsWith("/")) "" else "/", authority)
+    }
+
+    val service = Executors.newSingleThreadExecutor
+    val context: AuthenticationContext =  new AuthenticationContext(aadAuthorityUri, true, service)
+    PublicClientApplication.builder(CLIENT_ID).authority(aadAuthorityUri).build
+  }
+
+  def acquireAccessTokenUsingDeviceCodeFlow(clusterUrl: String, authority: String = "common", userDeviceCode: Option[DeviceCode] = None) : String = {
+    val deviceCode = if (userDeviceCode.isDefined) userDeviceCode.get else acquireDeviceCode(clusterUrl, authority)
+    val aadAuthorityUri = s"https://login.microsoftonline.com/$authority"
+    val service: ExecutorService =
+      Executors.newSingleThreadExecutor
+    val context: AuthenticationContext =  new AuthenticationContext(aadAuthorityUri, true, service)
+
+    val future: String = app.acquireToken(DeviceCodeFlowParameters.builder(scope, deviceCodeConsumer).build)
+    var text = null
+    Try {
+      val clipboard = Toolkit.getDefaultToolkit.getSystemClipboard
+      val dataFlavor = DataFlavor.stringFlavor
+      if (clipboard.isDataFlavorAvailable(dataFlavor)) {
+        text = clipboard.getData(dataFlavor).asInstanceOf[String]
+      }
+      clipboard.setContents(new StringSelection(deviceCode.getUserCode), null)
+    }
+
+    println(deviceCode.getMessage +
+      (if (!Desktop.isDesktopSupported) deviceCode
+      else " device code is already copied to clipboard - just press ctrl+v in the web"))
+    if (Desktop.isDesktopSupported) Desktop.getDesktop.browse(new URI(deviceCode.getVerificationUrl))
+    val result = waitAndAcquireTokenByDeviceCode(deviceCode, context)
+    if(text != null) {
+      Try {
+        val clipboard = Toolkit.getDefaultToolkit.getSystemClipboard
+        clipboard.setContents(new StringSelection(text), null)
+      }
+    }
+    if (result == null) throw new ServiceUnavailableException("authentication result was null")
+    result.getAccessToken
+  }
+
+  @throws[InterruptedException]
+  private def waitAndAcquireTokenByDeviceCode(deviceCode: DeviceCode, context: AuthenticationContext): AuthenticationResult = {
+    var timeout = 15 * 1000
+    var result: AuthenticationResult = null
+
+    // Logging is set to Fatal as root
+    val prevLevel = Logger.getLogger(classOf[AuthenticationContext]).getLevel
+    Thread.sleep(5000)
+    Logger.getLogger(classOf[AuthenticationContext]).setLevel(Level.FATAL)
+    while ( result == null && timeout > 0) {
+      try {
+        result = context.acquireTokenByDeviceCode(deviceCode, null).get
+
+      } catch {
+        case e: ExecutionException =>
+          Thread.sleep(1000)
+          timeout -= 1000
+      }
+    }
+
+    Logger.getLogger(classOf[AuthenticationContext]).setLevel(prevLevel)
+    result
+  }
+}
+
+// This class helps using device authentication in pyspark
+class DeviceAuthentication(val clusterUrl: String, val authority: String = "common") {
+  import DeviceAuthentication._
+
+  var currentDeviceCode: Option[DeviceCode] = None
+
+  def getDeviceCodeMessage: String = {
+    if (currentDeviceCode.isEmpty) {
+      refreshDeviceCode()
+    }
+
+    currentDeviceCode.get.getMessage
+  }
+
+  def acquireToken(): String = {
+    if (currentDeviceCode.isEmpty) {
+      refreshDeviceCode()
+    }
+
+    acquireAccessTokenUsingDeviceCodeFlow(clusterUrl, authority, currentDeviceCode)
+  }
+
+  def refreshDeviceCode(): Unit = {
+    currentDeviceCode = Some(acquireDeviceCode(clusterUrl, authority))
+  }
+}
