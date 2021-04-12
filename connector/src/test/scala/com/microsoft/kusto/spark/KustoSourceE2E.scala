32a2c7175b83da7d5ff7398fde5d29abb1527eec
ohad bitton <32278684+ohadbitt@users.noreply.github.com>
2021-04-12 21:01:54 +0300

    e2e

--- connector/src/test/scala/com/microsoft/kusto/spark/KustoSourceE2E.scala
+++ connector/src/test/scala/com/microsoft/kusto/spark/KustoSourceE2E.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicInteger
 
 import com.microsoft.azure.kusto.data.ClientFactory
 import com.microsoft.azure.kusto.data.auth.ConnectionStringBuilder
-import com.microsoft.kusto.spark.datasink.KustoSinkOptions
+import com.microsoft.kusto.spark.datasink.{KustoSinkOptions, SinkTableCreationMode}
 import com.microsoft.kusto.spark.datasource.{KustoSourceOptions, ReadMode}
 import com.microsoft.kusto.spark.sql.extension.SparkExtension._
 import com.microsoft.kusto.spark.utils.CslCommandsGenerator._
@@ -49,6 +49,13 @@ class KustoSourceE2E extends FlatSpec with BeforeAndAfterAll {
 
   private val loggingLevel: Option[String] = Option(System.getProperty("logLevel"))
   if (loggingLevel.isDefined) KDSU.setLoggingLevel(loggingLevel.get)
+  import spark.implicits._
+
+  val rowId = new AtomicInteger(1)
+  def newRow(): String = s"row-${rowId.getAndIncrement()}"
+  val expectedNumberOfRows: Int =  100
+  val rows: immutable.IndexedSeq[(String, Int)] = (1 to expectedNumberOfRows).map(v => (newRow(), v))
+  val dfOrig = rows.toDF("name", "value")
 
   "KustoSource" should "execute a read query on Kusto cluster in single mode" taggedAs KustoE2E in {
     val table: String = System.getProperty(KustoSinkOptions.KUSTO_TABLE)
@@ -68,31 +75,25 @@ class KustoSourceE2E extends FlatSpec with BeforeAndAfterAll {
     val table: String = System.getProperty(KustoSinkOptions.KUSTO_TABLE)
     val query: String = System.getProperty(KustoSourceOptions.KUSTO_QUERY, s"$table | where (toint(ColB) % 1 == 0)")
 
-    val storageAccount: String = System.getProperty("storageAccount")
-    val container: String = System.getProperty("container")
-    val blobKey: String = System.getProperty("blobKey")
-//    val blobSas: String = System.getProperty("blobSas")
+//    val storageAccount: String = System.getProperty("storageAccount")
+//    val container: String = System.getProperty("container")
+//    val blobKey: String = System.getProperty("blobKey")
+    val blobSas: String = System.getProperty("blobSas")
 
     val conf: Map[String, String] = Map(
       KustoSourceOptions.KUSTO_AAD_APP_ID -> appId,
       KustoSourceOptions.KUSTO_AAD_APP_SECRET -> appKey,
-      KustoSourceOptions.KUSTO_BLOB_STORAGE_ACCOUNT_NAME -> storageAccount,
-      KustoSourceOptions.KUSTO_BLOB_STORAGE_ACCOUNT_KEY -> blobKey,
-      KustoSourceOptions.KUSTO_BLOB_CONTAINER -> container
+      KustoSourceOptions.KUSTO_BLOB_STORAGE_SAS_URL -> blobSas
+      //      KustoSourceOptions.KUSTO_BLOB_STORAGE_ACCOUNT_NAME -> storageAccount,
+//      KustoSourceOptions.KUSTO_BLOB_STORAGE_ACCOUNT_KEY -> blobKey,
+//      KustoSourceOptions.KUSTO_BLOB_CONTAINER -> container
     )
 
     spark.read.kusto(cluster, database, query, conf).show(20)
   }
 
   "KustoConnector" should "write to a kusto table and read it back in default mode" taggedAs KustoE2E in {
-    import spark.implicits._
-
-    val rowId = new AtomicInteger(1)
-    def newRow(): String = s"row-${rowId.getAndIncrement()}"
-    val expectedNumberOfRows: Int =  100
-    val rows: immutable.IndexedSeq[(String, Int)] = (1 to expectedNumberOfRows).map(v => (newRow(), v))
-    val dfOrig = rows.toDF("name", "value")
-    val table = KustoQueryUtils.simplifyName(s"KustoSparkReadWriteTest_${UUID.randomUUID()}")
+     val table = KustoQueryUtils.simplifyName(s"KustoSparkReadWriteTest_${UUID.randomUUID()}")
 
     // Create a new table.
     val engineKcsb = ConnectionStringBuilder.createWithAadApplicationCredentials(s"https://$cluster.kusto.windows.net", appId, appKey, authority)
@@ -121,10 +122,53 @@ class KustoSourceE2E extends FlatSpec with BeforeAndAfterAll {
     val orig = dfOrig.select("name", "value").rdd.map(x => (x.getString(0), x.getInt(1))).collect().sortBy(_._2)
     val result = dfResult.select("ColA", "ColB").rdd.map(x => (x.getString(0), x.getInt(1))).collect().sortBy(_._2)
 
-
     assert(orig.deep == result.deep)
 
-    // Cleanup
+    // Cleanup TODO - drop in afterAll
     KustoTestUtils.tryDropAllTablesByPrefix(kustoAdminClient, database, "KustoSparkReadWriteTest")
+  }
+
+  "KustoSource" should "read distributed, transient cache change the filter but execute once" taggedAs KustoE2E in {
+    import spark.implicits._
+    val table = KustoQueryUtils.simplifyName(s"KustoSparkReadWriteTest_${UUID.randomUUID()}")
+    val engineKcsb: ConnectionStringBuilder = ConnectionStringBuilder.createWithAadApplicationCredentials(s"https://$cluster" +
+      s".kusto.windows.net", appId, appKey, authority)
+    val kustoAdminClient = ClientFactory.createClient(engineKcsb)
+
+    val conf: Map[String, String] = Map(
+      KustoSourceOptions.KUSTO_READ_MODE -> ReadMode.ForceDistributedMode.toString,
+      KustoSourceOptions.KUSTO_DISTRIBUTED_READ_MODE_TRANSIENT_CACHE -> true.toString,
+      KustoSourceOptions.KUSTO_AAD_APP_ID -> appId,
+      KustoSourceOptions.KUSTO_AAD_APP_SECRET -> appKey,
+      KustoSourceOptions.KUSTO_AAD_AUTHORITY_ID -> authority
+    )
+
+    // write
+    dfOrig.write
+      .format("com.microsoft.kusto.spark.datasource")
+      .option(KustoSinkOptions.KUSTO_CLUSTER, cluster)
+      .option(KustoSinkOptions.KUSTO_DATABASE, database)
+      .option(KustoSinkOptions.KUSTO_TABLE, table)
+      .option(KustoSinkOptions.KUSTO_AAD_APP_ID, appId)
+      .option(KustoSinkOptions.KUSTO_AAD_APP_SECRET, appKey)
+      .option(KustoSinkOptions.KUSTO_AAD_AUTHORITY_ID, authority)
+      .option(KustoSinkOptions.KUSTO_TABLE_CREATE_OPTIONS, SinkTableCreationMode.CreateIfNotExist.toString)
+      .mode(SaveMode.Append)
+      .save()
+
+    val df = spark.read.kusto("ohadprod.westeurope", database, table, conf)
+
+    assert(df.count() == expectedNumberOfRows)
+    assert(df.count() == expectedNumberOfRows)
+
+    val res3 = kustoAdminClient.execute(s""".show commands | where StartedOn > ago(2m)  | where CommandType ==
+      "DataExportToFile" | where Text has "$table"""")
+
+    val df2 = df.where($"value">50)
+    assert(df2.count() == 50)
+
+    // Should take up to another 10 seconds for .show commands to come up
+    Thread.sleep(10000)
+    assert(res3.getPrimaryResults.count() == 1)
   }
 }
