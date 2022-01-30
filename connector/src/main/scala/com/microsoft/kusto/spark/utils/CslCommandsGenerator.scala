c027b20b5c22b75355c7c989cdcd254bf3dcf667
Yihezkel Schoenbrun <yischoen@microsoft.com>
2022-01-30 22:38:33 +0200

    Fix command (SetNewIngestionTime is part of 'with')

--- connector/src/main/scala/com/microsoft/kusto/spark/utils/CslCommandsGenerator.scala
+++ connector/src/main/scala/com/microsoft/kusto/spark/utils/CslCommandsGenerator.scala
@@ -61,7 +61,7 @@ private[kusto] object CslCommandsGenerator {
   }
 
   def generateTableMoveExtentsCommand(sourceTableName: String, destinationTableName: String, batchSize: Int, isDestinationTableMaterializedViewSource: Boolean = false): String = {
-    val setNewIngestionTime: String = if (isDestinationTableMaterializedViewSource) "SetNewIngestionTime=true" else ""
+    val setNewIngestionTime: String = if (isDestinationTableMaterializedViewSource) "with(SetNewIngestionTime=true)" else ""
     s""".move extents to table $destinationTableName $setNewIngestionTime <|
        .show table $sourceTableName extents with(extentsShowFilteringRuntimePolicy='{"MaximumResultsCount":$batchSize}');
         $$command_results
@@ -70,7 +70,7 @@ private[kusto] object CslCommandsGenerator {
 
   def generateTableMoveExtentsAsyncCommand(sourceTableName: String, destinationTableName: String, batchSize: Int, isDestinationTableMaterializedViewSource: Boolean = false): String
   = {
-    val setNewIngestionTime: String = if (isDestinationTableMaterializedViewSource) "SetNewIngestionTime=true" else ""
+    val setNewIngestionTime: String = if (isDestinationTableMaterializedViewSource) "with(SetNewIngestionTime=true)" else ""
     s""".move async extents to table $destinationTableName $setNewIngestionTime <|
        .show table $sourceTableName extents with(extentsShowFilteringRuntimePolicy='{"MaximumResultsCount":$batchSize}');
        """
