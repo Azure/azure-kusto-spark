34a881d89a3ae111edc9d017876531b5b0033311
ohad bitton <32278684+ohadbitt@users.noreply.github.com>
2022-05-31 19:56:56 +0300

    Update samples/src/main/python/SynapseSample.py
    Co-authored-by: Yihezkel Schoenbrun <yihezkel@schoenbrun.net>

--- samples/src/main/python/SynapseSample.py
+++ samples/src/main/python/SynapseSample.py
@@ -59,7 +59,7 @@ df.write \
     .option("spark.synapse.linkedService", "<link service name>") \
     .option("kustoDatabase", "<Database name>") \
     .option("kustoTable", "<Table name>") \
-   #.option("authType", "LS") \ # if this added it uses the link service credentials, default is native - that will take the logged-in user credentials or the Synapse Workspace MSI for automated excution.            
+    #.option("authType", "LS") \ # If this is added, it uses the link service credentials. Otherwise, the default is native - using the logged-in user's credentials or the Synapse Workspace managed identity for automated execution.
     .mode("Append") \
     .save()
 
