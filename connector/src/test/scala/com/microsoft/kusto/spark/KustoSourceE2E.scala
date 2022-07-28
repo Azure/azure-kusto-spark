270f72914d1d989ca4803c0df6fd824cf11e663d
Ohad Bitton <ohbitton@microsoft.com>
2022-07-28 15:34:21 +0300

    e2e try 4

--- connector/src/test/scala/com/microsoft/kusto/spark/KustoSourceE2E.scala
+++ connector/src/test/scala/com/microsoft/kusto/spark/KustoSourceE2E.scala
@@ -29,28 +29,17 @@ class KustoSourceE2E extends FlatSpec with BeforeAndAfterAll {
 
   private var sc: SparkContext = _
   private var sqlContext: SQLContext = _
-  println("e2e: System.getenv")
-  println(System.getenv())
-  println("e2e: System.getProperties")
-  println(System.getProperties)
-
-  KDSU.logError("e2es", System.getProperties.toString)
-  KDSU.logError("e2es", System.getenv().toString)
-  var appId: String = System.getProperty(KustoSinkOptions.KUSTO_AAD_APP_ID)
-  var appKey: String = System.getProperty(KustoSinkOptions.KUSTO_AAD_APP_SECRET)
-  val authority: String = System.getProperty(KustoSinkOptions.KUSTO_AAD_AUTHORITY_ID, "microsoft.com")
-  val cluster: String = System.getProperty(KustoSinkOptions.KUSTO_CLUSTER)
-  val database: String = System.getProperty(KustoSinkOptions.KUSTO_DATABASE)
-  val table: String = System.getProperty(KustoSinkOptions.KUSTO_TABLE)
-  if (appId == null) {
-    var appIds = System.getenv(KustoSinkOptions.KUSTO_AAD_APP_ID)
-    if (appIds == null) {
-      println("e2e: KustoSinkOptions.KUSTO_AAD_APP_ID null")
-    }
-  }
+  KDSU.logError("System.getProperties", System.getProperties.toString)
+  KDSU.logError("System.getenv", System.getenv().toString)
+  var appId: String = System.getenv(KustoSinkOptions.KUSTO_AAD_APP_ID)
+  var appKey: String = System.getenv(KustoSinkOptions.KUSTO_AAD_APP_SECRET)
+  var authority: String = System.getenv(KustoSinkOptions.KUSTO_AAD_AUTHORITY_ID)
+  if (authority == null) authority = "microsoft.com"
+  val cluster: String = System.getenv(KustoSinkOptions.KUSTO_CLUSTER)
+  val database: String = System.getenv(KustoSinkOptions.KUSTO_DATABASE)
+  val table: String = System.getenv(KustoSinkOptions.KUSTO_TABLE)
   if (appKey == null) {
-    var secretPath = System.getProperty("SecretPath")
-    if (secretPath == null) secretPath = System.getenv("SecretPath")
+    val secretPath = System.getenv("SecretPath")
     if (secretPath == null) throw new IllegalArgumentException("SecretPath is not set")
     appKey = Files.readAllLines(Paths.get(secretPath)).get(0)
   }
