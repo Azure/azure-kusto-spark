afd39b2a7e77fd96b76e6ceffe60db6bb26c1cef
Ramachandran G <ramacg@microsoft.com>
2022-10-11 16:58:53 +0530

    * List files

--- connector/src/main/scala/com/microsoft/kusto/spark/datasink/KustoParquetWriter.scala
+++ connector/src/main/scala/com/microsoft/kusto/spark/datasink/KustoParquetWriter.scala
@@ -2,14 +2,17 @@ package com.microsoft.kusto.spark.datasink
 
 import com.microsoft.kusto.spark.datasource.TransientStorageCredentials
 import com.microsoft.kusto.spark.utils.{KustoAzureFsSetupCache, KustoQueryUtils}
+import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.io.IOUtils
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.{SparkContext, TaskContext}
 import org.joda.time.{DateTime, DateTimeZone}
 
+import java.net.URI
 import java.time.Instant
 import java.util.UUID
+import scala.collection.mutable.ListBuffer
 import scala.util.{Failure, Success, Try}
 
 /*
@@ -22,45 +25,43 @@ class KustoParquetWriter(sparkSession: SparkSession, storageCredentials: Transie
     // Create the blob file path
     val blobName = s"${KustoQueryUtils.simplifyName(databaseName)}_${tmpTableName}_" +
       s"${UUID.randomUUID.toString}_${TaskContext.getPartitionId()}_spark.parquet"
-    val blobPath = s"wasbs://${storageCredentials.blobContainer}@${storageCredentials.storageAccountName}." +
-      s"blob.${storageCredentials.domainSuffix}/$blobName"
-    val blobDestinationPath = s"wasbs://${storageCredentials.blobContainer}@${storageCredentials.storageAccountName}." +
-      s"blob.${storageCredentials.domainSuffix}/$blobName-merged-parquet"
+    val blobRoot = s"wasbs://${storageCredentials.blobContainer}@${storageCredentials.storageAccountName}.blob.${storageCredentials.domainSuffix}"
+    val blobPath = s"/$blobName"
     // Set up the access in spark config
-    setUpAccessForAzureFS()
+    val hadoopConfig = getWasbHadoopConfig(sparkSession.sparkContext.hadoopConfiguration)
+    setUpAccessForAzureFS(hadoopConfig)
     // Write the file
-    // inputDataFrame.write.format("com.microsoft.kusto.spark.datasink.parquet.KustoParquetSinkProvider").option("create_table", "true").save(blobPath)
-
     println(s"Starting ${Instant.now()}")
-
-    inputDataFrame.write.parquet(blobPath)
-
+    inputDataFrame.write.parquet(s"$blobRoot$blobPath")
     println(s"Processing next at ${Instant.now()}")
-
-    // CloudBlockBlob
-    val listOfFiles = Seq("","","")
-
-    import sparkSession.implicits._
-    val files = listOfFiles.toDF()
-    files.foreachPartition(file=>{
-
-    })
-
-//    sparkContext.binaryFiles(blobPath).foreachPartition(partition=>{
-//      partition.foreach(part=>{
-//        val timeNow = Instant.now()
-//        println(s"$timeNow :: Partitioned Files ==> ${part._1}")
-//      })
-//    })
+    val hdfs = FileSystem.get(new URI(blobRoot),hadoopConfig)
+    val sourcePath = new Path(blobPath)
+    val listOfFiles = new ListBuffer[String]()
+    Try(hdfs.listFiles(sourcePath, true)) match {
+      case Success(remoteFileIterator) => {
+        while (remoteFileIterator.hasNext){
+          val remoteFile = remoteFileIterator.next()
+          listOfFiles.append(remoteFile.getPath.getName)
+        }
+      }
+      case Failure(exception) => exception.printStackTrace()
+    }
+    println(s"List of files ${listOfFiles.mkString(",")}")
   }
 
-  private[kusto] def setUpAccessForAzureFS(): Unit = {
-    val hadoopConfiguration = sparkContext.hadoopConfiguration
+  private[kusto] def getWasbHadoopConfig(hadoopConfig:Configuration): Configuration = {
+    hadoopConfig.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
+    hadoopConfig.set("fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
+    hadoopConfig.set("fs.wasb.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
+    hadoopConfig
+  }
+
+  private[kusto] def setUpAccessForAzureFS(hadoopConfig:Configuration): Unit = {
     val now = DateTime.now(DateTimeZone.UTC)
     if (!storageCredentials.sasDefined) {
       if (!KustoAzureFsSetupCache.updateAndGetPrevStorageAccountAccess(storageCredentials.storageAccountName
         , storageCredentials.storageAccountKey, now)) {
-        hadoopConfiguration.set(s"fs.azure.account.key.${storageCredentials.storageAccountName}." +
+        hadoopConfig.set(s"fs.azure.account.key.${storageCredentials.storageAccountName}." +
           s"blob.${storageCredentials.domainSuffix}",
           s"${storageCredentials.storageAccountKey}")
       }
@@ -68,14 +69,10 @@ class KustoParquetWriter(sparkSession: SparkSession, storageCredentials: Transie
     else {
       if (!KustoAzureFsSetupCache.updateAndGetPrevSas(storageCredentials.blobContainer,
         storageCredentials.storageAccountName, storageCredentials.sasKey, now)) {
-        hadoopConfiguration.set(s"fs.azure.sas.${storageCredentials.blobContainer}.${storageCredentials.storageAccountName}." +
+        hadoopConfig.set(s"fs.azure.sas.${storageCredentials.blobContainer}.${storageCredentials.storageAccountName}." +
           s"blob.${storageCredentials.domainSuffix}",
           s"${storageCredentials.sasKey}")
       }
     }
-
-    hadoopConfiguration.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
-    hadoopConfiguration.set("fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
-    hadoopConfiguration.set("fs.wasb.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
   }
 }
