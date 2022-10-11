354ad7128f9cbe06649edcca28a70b44d4f2f9c5
Abhishek Saharn <asaharn@microsoft.com>
2022-10-11 19:20:19 +0530

    Added ingestor class

---
+++ connector/src/main/scala/com/microsoft/kusto/spark/datasink/KustoParquetIngestor.scala
@@ -0,0 +1,20 @@
+package com.microsoft.kusto.spark.datasink
+
+import com.microsoft.azure.kusto.data.auth.ConnectionStringBuilder
+import com.microsoft.azure.kusto.ingest.{IngestClient, IngestClientFactory, IngestionProperties}
+import com.microsoft.azure.kusto.ingest.source.BlobSourceInfo
+
+class KustoParquetIngestor(ingestionProperties: IngestionProperties ) extends Serializable {
+
+  def ingest(blobPath : String) : Unit = {
+    val csb: ConnectionStringBuilder = ConnectionStringBuilder.createWithAadApplicationCredentials("https://ingest-sdke2etestcluster.eastus.dev.kusto.windows.net",
+      "",
+      "",
+      "")
+    val ingestClient: IngestClient = IngestClientFactory.createClient(csb)
+    println(s" ==========>>>>>>>>>>>>>>>> IN INGESTOR : >>>>>>>>>>  ${blobPath} ")
+    val blobSourceInfo = new BlobSourceInfo(blobPath)
+    ingestClient.ingestFromBlob(blobSourceInfo, ingestionProperties)
+  }
+
+}
