94be50664888a509e9265c609b10a8e6f7d7d408
Ramachandran G <ramacg@microsoft.com>
2022-10-11 19:52:26 +0530

    * Fix task serialization issue

--- connector/src/main/scala/com/microsoft/kusto/spark/datasink/KustoParquetWriter.scala
+++ connector/src/main/scala/com/microsoft/kusto/spark/datasink/KustoParquetWriter.scala
@@ -1,16 +1,15 @@
 package com.microsoft.kusto.spark.datasink
 
+import com.microsoft.azure.kusto.ingest.IngestionProperties
 import com.microsoft.kusto.spark.datasource.TransientStorageCredentials
-import com.microsoft.kusto.spark.utils.{KustoAzureFsSetupCache, KustoQueryUtils}
+import com.microsoft.kusto.spark.utils.{KustoAzureFsSetupCache, KustoDataSourceUtils, KustoQueryUtils}
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.hadoop.io.IOUtils
+import org.apache.spark.TaskContext
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.{SparkContext, TaskContext}
 import org.joda.time.{DateTime, DateTimeZone}
 
 import java.net.URI
-import java.time.Instant
 import java.util.UUID
 import scala.collection.mutable.ListBuffer
 import scala.util.{Failure, Success, Try}
@@ -19,6 +18,8 @@ import scala.util.{Failure, Success, Try}
   Serializes and writes data as a parquet blob
 */
 class KustoParquetWriter(sparkSession: SparkSession, storageCredentials: TransientStorageCredentials) {
+  private val className = this.getClass.getName
+
   def write(inputDataFrame: DataFrame,
             databaseName: String,
             tmpTableName: String): Unit = {
@@ -31,32 +32,45 @@ class KustoParquetWriter(sparkSession: SparkSession, storageCredentials: Transie
     val hadoopConfig = getWasbHadoopConfig(sparkSession.sparkContext.hadoopConfiguration)
     setUpAccessForAzureFS(hadoopConfig)
     // Write the file
-    println(s"Starting ${Instant.now()}")
     inputDataFrame.write.parquet(s"$blobRoot$blobPath")
-    println(s"Processing next at ${Instant.now()}")
-    val hdfs = FileSystem.get(new URI(blobRoot),hadoopConfig)
+    // Get the list of files created. This will be used for ingestion
+    val hdfs = FileSystem.get(new URI(blobRoot), hadoopConfig)
     val sourcePath = new Path(blobPath)
     val listOfFiles = new ListBuffer[String]()
+    // List these files and add them to the list for processing
     Try(hdfs.listFiles(sourcePath, true)) match {
-      case Success(remoteFileIterator) => {
-        while (remoteFileIterator.hasNext){
-          val remoteFile = remoteFileIterator.next()
-          listOfFiles.append(remoteFile.getPath.getName)
+      case Success(remoteFileIterator) =>
+        while (remoteFileIterator.hasNext) {
+          val mayBeRemoteFile = Option(remoteFileIterator.next())
+          mayBeRemoteFile match {
+            case Some(remoteFile) => if (!"_SUCCESS".equals(remoteFile.getPath.getName)) listOfFiles.append(remoteFile.getPath.getName)
+            case None => KustoDataSourceUtils.logInfo(className, s"Empty paths while parsing $sourcePath. " +
+              "Will not be added for processing")
+          }
         }
-      }
-      case Failure(exception) => exception.printStackTrace()
+      // Cannot list these files. Error accessing the file system needs to be halted processing
+      case Failure(exception) => throw exception
     }
-    println(s"List of files ${listOfFiles.mkString(",")}")
+    import sparkSession.implicits._
+    val listOfFilesToProcess = listOfFiles.toDF()
+    listOfFilesToProcess.rdd.foreachPartition(partitionResult => {
+      val ingestionProperties = new IngestionProperties("", "")
+      val parquetIngestor = new KustoParquetIngestor(ingestionProperties)
+      partitionResult.foreach(rowIterator => {
+        val fileName = rowIterator.getString(0)
+        parquetIngestor.ingest(fileName)
+      })
+    })
   }
 
-  private[kusto] def getWasbHadoopConfig(hadoopConfig:Configuration): Configuration = {
+  private[kusto] def getWasbHadoopConfig(hadoopConfig: Configuration): Configuration = {
     hadoopConfig.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
     hadoopConfig.set("fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
     hadoopConfig.set("fs.wasb.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
     hadoopConfig
   }
 
-  private[kusto] def setUpAccessForAzureFS(hadoopConfig:Configuration): Unit = {
+  private[kusto] def setUpAccessForAzureFS(hadoopConfig: Configuration): Unit = {
     val now = DateTime.now(DateTimeZone.UTC)
     if (!storageCredentials.sasDefined) {
       if (!KustoAzureFsSetupCache.updateAndGetPrevStorageAccountAccess(storageCredentials.storageAccountName
