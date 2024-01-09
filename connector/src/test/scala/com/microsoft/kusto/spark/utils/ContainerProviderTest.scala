52d8b4be9d90c6491ead22cdf9c2a658ce4f76c0
Ramachandran A G <ramacg@microsoft.com>
2024-01-09 08:07:56 +0530

    * Remove irrelvant tests * Fix ignored tests

--- connector/src/test/scala/com/microsoft/kusto/spark/utils/ContainerProviderTest.scala
+++ connector/src/test/scala/com/microsoft/kusto/spark/utils/ContainerProviderTest.scala
@@ -1,41 +1,84 @@
 package com.microsoft.kusto.spark.utils
 
-import com.microsoft.azure.kusto.data.KustoOperationResult
+import com.azure.storage.blob.BlobContainerClient
+import com.microsoft.azure.kusto.data.auth.ConnectionStringBuilder
 import com.microsoft.azure.kusto.data.exceptions.DataServiceException
-import org.apache.commons.lang3.exception.ExceptionUtils
+import com.microsoft.azure.kusto.data.{Client, ClientRequestProperties, KustoOperationResult}
+import com.microsoft.azure.kusto.ingest.exceptions.IngestionServiceException
+import com.microsoft.azure.kusto.ingest.resources.ContainerWithSas
+import com.microsoft.azure.kusto.ingest.{IngestionResourceManager, QueuedIngestClient}
 import org.apache.http.HttpHost
 import org.apache.http.conn.HttpHostConnectException
+import org.mockito.Mockito
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.Ignore
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import java.net.{ConnectException, InetAddress}
-import scala.io.Source
 import java.io.IOException
+import java.net.{ConnectException, InetAddress}
+import java.util.Collections
+import scala.collection.JavaConverters.seqAsJavaListConverter
+import scala.io.Source
 
-@Ignore
+
 class ContainerProviderTest extends AnyFlatSpec with Matchers with MockFactory {
   val CACHE_EXPIRY_SEC = 2
   val SLEEP_TIME_SEC = 10
+
+  private def createExtendedKustoMockClient(hasEmptyResults: Boolean = false, mockDmClient: Client,
+                                            mayBeExceptionThrown: Option[Throwable] = None): ExtendedKustoClient = {
+    val mockIngestClient: QueuedIngestClient = mock[QueuedIngestClient]
+    val mockIngestionResourceManager: IngestionResourceManager = Mockito.mock[IngestionResourceManager](classOf[IngestionResourceManager])
+
+    mayBeExceptionThrown match {
+      case Some(exception) => Mockito.when(mockIngestionResourceManager.getShuffledContainers)
+        .thenThrow(exception).thenThrow(exception).
+        thenAnswer(_ => List(getMockContainerWithSas(1), getMockContainerWithSas(2)).asJava)
+      case None => if (hasEmptyResults) {
+        Mockito.when(mockIngestionResourceManager.getShuffledContainers).
+          thenAnswer(_ => Collections.EMPTY_LIST)
+      } else {
+        Mockito.when(mockIngestionResourceManager.getShuffledContainers).
+          thenAnswer(_ => List(getMockContainerWithSas(1), getMockContainerWithSas(2)).asJava)
+      }
+    }
+    // noMoreThanTwice()
+    mockIngestClient.getResourceManager _ expects() returning mockIngestionResourceManager
+    // Unfortunately we cannot Mock this class as there is a member variable that is a val and cannot be mocked
+    new ExtendedKustoClient(new ConnectionStringBuilder("https://somecluster.eastus.kusto.windows.net/"),
+      new ConnectionStringBuilder("https://ingest-somecluster.eastus.kusto.windows.net"), "somecluster") {
+      override lazy val ingestClient: QueuedIngestClient = mockIngestClient
+      override lazy val dmClient: Client = mockDmClient
+    }
+  }
+
+  private def getMockContainerWithSas(index: Int): ContainerWithSas = {
+    val mockResultsOne: ContainerWithSas = Mockito.mock[ContainerWithSas](classOf[ContainerWithSas])
+    val blobResultsOne: BlobContainerClient = Mockito.mock[BlobContainerClient](classOf[BlobContainerClient])
+    Mockito.when(blobResultsOne.getBlobContainerUrl).thenAnswer(_ => s"https://sacc$index.blob.core.windows.net/20230430-ingestdata-e5c334ee145d4b4-0")
+    Mockito.when(mockResultsOne.getSas).thenAnswer(_ => "?sv=2018-03-28&sr=c&sp=rw")
+    Mockito.when(mockResultsOne.getContainer).thenAnswer(_ => blobResultsOne)
+    mockResultsOne
+  }
   // happy path
   "ContainerProvider" should "return a container" in {
-    val extendedMockClient = mock[ExtendedKustoClient]
-    val extendedMockClientEmptyFail = mock[ExtendedKustoClient]
     val kustoOperationResult = new KustoOperationResult(readTestSource("storage-result.json"), "v1")
+    val mockDmClient = mock[Client]
+    (mockDmClient.execute(_: String, _: String, _: ClientRequestProperties)).expects(*, *, *).
+      noMoreThanOnce().returning(kustoOperationResult)
+
     val clusterAlias = "ingest-cluster"
     val command = ".create tempstorage"
-    val ingestProviderEntryCreator = (c: ContainerAndSas) => c
     /*
       Invoke and test
      */
-    val containerProvider = new ContainerProvider(extendedMockClient, clusterAlias, command,CACHE_EXPIRY_SEC)
-    extendedMockClient.executeDM _ expects(command, None, *) noMoreThanOnce() returning kustoOperationResult
-    containerProvider.getContainer.containerUrl should(not be "")
+    val extendedMockClient = createExtendedKustoMockClient(mockDmClient = mockDmClient)
+    val containerProvider = new ContainerProvider(extendedMockClient, clusterAlias, command, CACHE_EXPIRY_SEC)
+    containerProvider.getContainer.containerUrl should (not be "")
     Some(containerProvider.getContainer.containerUrl) should contain oneOf
       ("https://sacc1.blob.core.windows.net/20230430-ingestdata-e5c334ee145d4b4-0",
         "https://sacc2.blob.core.windows.net/20230430-ingestdata-e5c334ee145d4b4-0")
-    containerProvider.getContainer.sas should(not be "")
+    containerProvider.getContainer.sas should (not be "")
 
     /* Second test that returns from cache. The test will fail if the client is invoked again as expectation is to call once */
     containerProvider.getContainer.containerUrl should (not be "")
@@ -43,90 +86,57 @@ class ContainerProviderTest extends AnyFlatSpec with Matchers with MockFactory {
       ("https://sacc1.blob.core.windows.net/20230430-ingestdata-e5c334ee145d4b4-0",
         "https://sacc2.blob.core.windows.net/20230430-ingestdata-e5c334ee145d4b4-0")
     containerProvider.getContainer.sas should (not be "")
-
-
     /* Third test where the cache expires and the invocation throws an exception */
     Thread.sleep(SLEEP_TIME_SEC * 1000) // Milliseconds
-    extendedMockClient.executeDM _ expects(command, None, *) throws new DataServiceException(clusterAlias,"Cannot create temp storage",false)
     containerProvider.getContainer.containerUrl should (not be "")
     Some(containerProvider.getContainer.containerUrl) should contain oneOf
       ("https://sacc1.blob.core.windows.net/20230430-ingestdata-e5c334ee145d4b4-0",
         "https://sacc2.blob.core.windows.net/20230430-ingestdata-e5c334ee145d4b4-0")
     containerProvider.getContainer.sas should (not be "")
-    // The case where storageUris.nonEmpty is false. This will throw the exception as there is nothing to give from the cache
-    Thread.sleep(SLEEP_TIME_SEC * 1000) // Milliseconds
 
-    extendedMockClientEmptyFail.executeDM _ expects(command, None, *) throws new DataServiceException(clusterAlias, "Cannot create temp storage", false)
-    val emptyStorageContainerProvider = new ContainerProvider(extendedMockClientEmptyFail, clusterAlias, command,CACHE_EXPIRY_SEC)
+    // The case where storageUris.nonEmpty is false. This will throw the exception as there is nothing to give from the cache
+    Thread.sleep((SLEEP_TIME_SEC * 2) * 1000) // Milliseconds
+
+    val mockDmFailClient = mock[Client]
+    val extendedMockClientEmptyFail = createExtendedKustoMockClient(hasEmptyResults = true, mockDmClient = mockDmFailClient)
+    val emptyStorageContainerProvider = new ContainerProvider(extendedMockClientEmptyFail, clusterAlias, command, CACHE_EXPIRY_SEC)
     val caught =
-      intercept[DataServiceException] { // Result type: Assertion
+      intercept[RuntimeException] { // Result type: Assertion
         emptyStorageContainerProvider.getContainer
-    }
-    assert(caught.getMessage.indexOf("Cannot create temp storage") != -1)
+      }
+    assert(caught.getMessage.indexOf("Failed to allocate temporary storage") != -1)
   }
 
   "ContainerProvider" should "fail in the case when call succeeds but returns no storage" in {
-    val extendedMockClient = mock[ExtendedKustoClient]
-    val kustoOperationResult = new KustoOperationResult(readTestSource("storage-result-empty.json"), "v1")
     val clusterAlias = "ingest-cluster"
     val command = ".create tempstorage"
-    val ingestProviderEntryCreator = (c: ContainerAndSas) => c
+
+    val kustoOperationResult = new KustoOperationResult(readTestSource("storage-result-empty.json"), "v1")
+    val mockDmClient = mock[Client]
+    (mockDmClient.execute(_: String, _: String, _: ClientRequestProperties)).expects(*, *, *).
+      noMoreThanOnce() returning (kustoOperationResult)
+    /*
+      Invoke and test
+     */
+    val extendedMockClient = createExtendedKustoMockClient(hasEmptyResults = true, mockDmClient = mockDmClient)
     /*
       Invoke and test. In this case the call succeeds but returns no storage. This will hit the empty storage block
      */
     val containerProvider = new ContainerProvider(extendedMockClient, clusterAlias, command, CACHE_EXPIRY_SEC)
-    extendedMockClient.executeDM _ expects(command, None, *) noMoreThanOnce() returning kustoOperationResult
     the[RuntimeException] thrownBy containerProvider.getContainer should have message "Failed to allocate temporary storage"
   }
 
   "ContainerProvider" should "retry and return a container in case of a temporary HTTPException" in {
-    val extendedMockClient = mock[ExtendedKustoClient]
-    val extendedMockNoRootExceptionFail = mock[ExtendedKustoClient]
-    val extendedMockClientIOExceptionFail = mock[ExtendedKustoClient]
-    val kustoOperationResult = new KustoOperationResult(readTestSource("storage-result.json"), "v1")
     val clusterAlias = "ingest-cluster"
     val command = ".get ingestion resources"
     /*
       Invoke and test
      */
+    val mockDmClient = mock[Client]
+    val extendedMockClient = createExtendedKustoMockClient(mockDmClient = mockDmClient,
+      mayBeExceptionThrown = Some(new IngestionServiceException("IOError when trying to retrieve CloudInfo")))
     val containerProvider = new ContainerProvider(extendedMockClient, clusterAlias, command, CACHE_EXPIRY_SEC)
-
-    // HttpHostConnectException -> IOException -> ConnectException This is the only hierarchy available
-    extendedMockClient.executeDM _ expects(command, None, *) throws new DataServiceException(clusterAlias,
-      "IOError when trying to retrieve CloudInfo",new HttpHostConnectException(
-      new IOException(new ConnectException("Connection timed out")),
-      HttpHost.create("kustocluster.centralus.kusto.windows.net"),
-      InetAddress.getLoopbackAddress),true) once() returning kustoOperationResult
-    // The first call will fail with a HttpHostConnectException. The second call will succeed
-    containerProvider.getContainer.containerUrl should (not be "")
-    Some(containerProvider.getContainer.containerUrl) should contain oneOf
-      ("https://sacc1.blob.core.windows.net/20230430-ingestdata-e5c334ee145d4b4-0",
-        "https://sacc2.blob.core.windows.net/20230430-ingestdata-e5c334ee145d4b4-0")
-    containerProvider.getContainer.sas should (not be "")
-
-    // Test where the root exception below it is empty, It will fail
-    extendedMockNoRootExceptionFail.executeDM _ expects(command, None, *) throws new DataServiceException(clusterAlias,
-      "No root exception", false)
-    val noRootExceptionContainerProvider = new ContainerProvider(extendedMockNoRootExceptionFail, clusterAlias, command,
-      CACHE_EXPIRY_SEC)
-    val caught =
-      intercept[DataServiceException] { // Result type: Assertion
-        noRootExceptionContainerProvider.getContainer
-      }
-    assert(caught.getMessage.indexOf("No root exception") != -1)
-
-
-    // Test where the root exception below it is not a HttpHostConnectException. It will fail
-    extendedMockClientIOExceptionFail.executeDM _ expects(command, None, *) throws new DataServiceException(clusterAlias,
-      "No root exception", new IOException(new ConnectException("IOError when trying to retrieve CloudInfo")), true)
-    val ioExceptionContainerProvider = new ContainerProvider(extendedMockClientIOExceptionFail, clusterAlias, command,
-      CACHE_EXPIRY_SEC)
-    val ioErrorCaught =
-      intercept[DataServiceException] { // Result type: Assertion
-        ioExceptionContainerProvider.getContainer
-      }
-    assert(ioErrorCaught.getMessage.indexOf("No root exception") != -1)
-    assert(ExceptionUtils.getRootCause(ioErrorCaught).getMessage.indexOf("IOError when trying to retrieve CloudInfo") != -1)
+    the[IngestionServiceException] thrownBy containerProvider.getContainer should have message "IOError when trying to retrieve CloudInfo"
   }
 
   private def readTestSource(fileName: String): String = {
