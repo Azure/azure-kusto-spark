094e96865ea1c5ce53c18aab4b46fc3b46427e8f
Ramachandran A G <106139410+ag-ramachandran@users.noreply.github.com>
2024-06-05 11:08:52 +0530

    * Fix license headers (#369)

--- connector/src/main/scala/com/microsoft/kusto/spark/datasink/KustoWriter.scala
+++ connector/src/main/scala/com/microsoft/kusto/spark/datasink/KustoWriter.scala
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.microsoft.kusto.spark.datasink
 
 import com.azure.storage.common.policy.{RequestRetryOptions, RetryPolicyType}
--- connector/src/test/scala/com/microsoft/kusto/spark/KustoFilterTests.scala
+++ connector/src/test/scala/com/microsoft/kusto/spark/KustoFilterTests.scala
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.microsoft.kusto.spark
 
 import com.microsoft.kusto.spark.datasource.{KustoFilter, KustoFiltering, KustoSchema}
