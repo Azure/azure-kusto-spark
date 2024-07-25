35d6da3611a8ab7e4e7cd9f159bdbcb6bfe4ba4b
Ramachandran G <ramacg@microsoft.com>
2024-07-25 15:14:47 +0530

    * Get a version working for E2E spark tests

--- connector/src/test/scala/com/microsoft/kusto/spark/it/E2ETestWithContainers.scala
+++ connector/src/test/scala/com/microsoft/kusto/spark/it/E2ETestWithContainers.scala
@@ -3,19 +3,22 @@
 
 package com.microsoft.kusto.spark.it
 
+import com.microsoft.kusto.spark.KustoTestUtils.getSystemTestOptions
+import com.microsoft.kusto.spark.datasink.KustoSinkOptions
 import com.microsoft.kusto.spark.utils.KustoDataSourceUtils
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.testcontainers.containers.{GenericContainer, Network}
-import org.testcontainers.images.builder.Transferable
 import org.testcontainers.utility.{DockerImageName, MountableFile}
+
+import java.util
 
 class E2ETestWithContainers extends AnyFlatSpec with Matchers with BeforeAndAfterAll {
   private val network = Network.newNetwork
   private val sparkContainer: GenericContainer[?] =
     new GenericContainer(DockerImageName.parse("bitnami/spark:3.1.2")).withNetwork(network)
-
+  private lazy val kustoTestConnectionOptions = getSystemTestOptions
   private val targetFolderName = "/opt/bitnami/spark/examples/jars/"
   private val jarFileBaseName = s"kusto-spark_3.0_2.12-${KustoDataSourceUtils.Version}"
   private val testJarFileToSubmit = s"$jarFileBaseName-tests.jar";
@@ -23,6 +26,12 @@ class E2ETestWithContainers extends AnyFlatSpec with Matchers with BeforeAndAfte
 
   override def beforeAll(): Unit = {
     sparkContainer.start()
+    val envVars = new util.HashMap[String, String]()
+    envVars.put(KustoSinkOptions.KUSTO_CLUSTER, kustoTestConnectionOptions.cluster)
+    envVars.put(KustoSinkOptions.KUSTO_ACCESS_TOKEN, kustoTestConnectionOptions.accessToken)
+    envVars.put(KustoSinkOptions.KUSTO_DATABASE, kustoTestConnectionOptions.database)
+    envVars.put(KustoSinkOptions.KUSTO_AAD_AUTHORITY_ID, kustoTestConnectionOptions.tenantId)
+    sparkContainer.withEnv(envVars)
     sparkContainer.copyFileToContainer(
       MountableFile.forHostPath(s"target/$testJarFileToSubmit"),
       s"$targetFolderName$testJarFileToSubmit")
@@ -32,10 +41,15 @@ class E2ETestWithContainers extends AnyFlatSpec with Matchers with BeforeAndAfte
   }
 
   "Submit a spark job to perform an ingest" should "ingest" in {
-    val commandToExec = s"spark-submit --jars $targetFolderName$jarFileClasspath  " +
-      s"--class com.microsoft.kusto.spark.it.KustoE2EMain $targetFolderName$testJarFileToSubmit"
+    val ivyOpts = "\"-Divy.cache.dir=/tmp -Divy.home=/tmp\""
+    val commandToExec =
+      s"spark-submit --conf spark.driver.extraJavaOptions=$ivyOpts " +
+        s"--packages org.apache.hadoop:hadoop-azure:3.3.4,com.microsoft.azure:azure-storage:8.6.6  --jars $targetFolderName$jarFileClasspath  " +
+        s"--class com.microsoft.kusto.spark.it.KustoE2EMain $targetFolderName$testJarFileToSubmit " +
+        s"${kustoTestConnectionOptions.cluster}  ${kustoTestConnectionOptions.database}   ${kustoTestConnectionOptions.accessToken} > /tmp/results.txt 2>&1"
     val execResult = sparkContainer.execInContainer("/bin/sh", "-c", commandToExec)
-    Thread.sleep(10000)
+    println(execResult.getStdout)
+    Thread.sleep(300000)
     execResult.getStdout should include("Pi is roughly")
   }
 
--- connector/src/test/scala/com/microsoft/kusto/spark/it/KustoE2EMain.scala
+++ connector/src/test/scala/com/microsoft/kusto/spark/it/KustoE2EMain.scala
@@ -3,16 +3,36 @@
 
 package com.microsoft.kusto.spark.it
 
-import scala.math.random
+import com.microsoft.kusto.spark.KustoTestUtils.getSystemTestOptions
+import com.microsoft.kusto.spark.datasink.{KustoSinkOptions, SinkTableCreationMode}
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
 
 object KustoE2EMain {
+  private val nofExecutors = 4
+  private lazy val kustoTestConnectionOptions = getSystemTestOptions
+
   def main(args: Array[String]): Unit = {
-    var count = 0
-    for (i <- 1 to 100000) {
-      val x = random() * 2 - 1
-      val y = random() * 2 - 1
-      if (x * x + y * y <= 1) count += 1
-    }
-    println(s"Pi is roughly ${4 * count / 100000.0}")
+    val wasbs_path =
+      "wasbs://nyctlc@azureopendatastorage.blob.core.windows.net/yellow/puYear=2018/puMonth=11"
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("KustoSink")
+      .config("fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
+      .master(f"local[$nofExecutors]")
+      .getOrCreate()
+    val nyctlc = spark.read.parquet(wasbs_path)
+
+    nyctlc.write
+      .format("com.microsoft.kusto.spark.datasource")
+      .option(KustoSinkOptions.KUSTO_CLUSTER, args(0))
+      .option(KustoSinkOptions.KUSTO_DATABASE, args(1))
+      .option(KustoSinkOptions.KUSTO_TABLE, "NYCYellowTaxi")
+      .option(KustoSinkOptions.KUSTO_ACCESS_TOKEN, args(2))
+      .option(
+        KustoSinkOptions.KUSTO_TABLE_CREATE_OPTIONS,
+        SinkTableCreationMode.CreateIfNotExist.toString)
+      .mode(SaveMode.Append)
+      .save()
   }
 }
